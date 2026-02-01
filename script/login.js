3e94ad1ef761c113dc8d96af05cbc70a96b41d74
Utsob1621 <utsobsaha9072@gmail.com>
2026-02-02 02:06:24 +0600

    checked account number length and also checkesd 4 digit pin number

--- script/login.js
+++ script/login.js
@@ -1,3 +1,23 @@
 document.getElementById("login-btn").addEventListener("click", function (event){
   event.preventDefault ();
-});
+
+
+    const accountNumber = document.getElementById("Account-number").value;
+    const pin = document.getElementById("Account-pin").value;
+    const convertPin = parseInt(pin);
+
+      if(accountNumber.length === 11){
+        if(convertPin === 1234){
+          window.location.href="./main.html";
+       }
+
+      else{
+        alert("invalid pin");
+       }
+     }
+
+     else{
+      alert("Enter the valid number");
+     }
+  }
+);
