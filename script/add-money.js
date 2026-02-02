1961e80143bd6a4cd5b052f05fd63714715f9c36
Utsob1621 <utsobsaha9072@gmail.com>
2026-02-02 13:42:13 +0600

    implement some utilities js function and share all js utilities function used add-money section and also implement add money to ain-balance

--- script/add-money.js
+++ script/add-money.js
@@ -1,13 +1,25 @@
-document.getElementById("add-money").addEventListener("click", 
-  function(event){
-  event.preventDefault();
+document.getElementById("add-money").addEventListener("click",
+  function (event) {
+    event.preventDefault();
 
-  const amount = getInputValueById("amount");
-  const pin = getInputValueById("Account-pin");
-  const account = document.getElementById("account-number").value;
-  const mainBalance = getInnerTextById("main-balance");
-  const selectedBank = document.getElementById("allBank").value;
+    const amount = getInputValueById("amount");
+    const pin = getInputValueById("Account-pin");
+    const account = document.getElementById("account-number").value;
+    const mainBalance = getInnerTextById("main-balance");
+    // const selectedBank = document.getElementById("allBank").value;
 
+    if (account.length === 11) {
+      if (pin === 4321) {
+        const sum = mainBalance + amount;
+        getInnerTextByIdAndValue("main-balance", sum);
+      }
 
+      else {
+        alert("Invalid pin");
+      }
+    }
+    else {
+      alert("Enter the valid number");
+    }
 
   });
