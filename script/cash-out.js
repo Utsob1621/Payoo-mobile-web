2e0f87b12a51fe90fbe12fbe1f1d5685455eb032
Utsob1621 <utsobsaha9072@gmail.com>
2026-02-02 22:11:08 +0600

    implement the add money transaction history function

--- script/cash-out.js
+++ script/cash-out.js
@@ -5,11 +5,25 @@ document.getElementById("cashOut-money").addEventListener("click",
     const amount = getInputValueById("cashOut-amount");
     const pinNumber = getInputValueById("cashOut-pin");
     const mainBalance = getInnerTextById("main-balance");
+    const selectedBank = document.getElementById("allBank").value;
+
 
     if (accountNumber.length === 11) {
       if (pinNumber === 4321) {
         const sum = mainBalance - amount;
         getInnerTextByIdAndValue("main-balance", sum);
+
+
+        const container = document.getElementById("transaction-history");
+
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <h1>bank ${selectedBank}<h1/>
+        <h3>added money ${amount}<h3/>
+        <p>from this ${account} number<p/>
+      `
+        container.appendChild(div);
+
       }
       else {
         alert("invalid pin");
